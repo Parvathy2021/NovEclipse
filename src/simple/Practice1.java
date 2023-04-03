c30ddbdd9efca7dc582ee1a87099f008e000c190
Parvathy2021 <parvathyvenu14@gmail.com>
2023-04-03 12:16:03 +0530

    First commit

---
+++ src/simple/Practice1.java
@@ -0,0 +1,23 @@
+package simple;
+
+public class Practice1 {
+
+	public static void main(String[] args) {
+		
+//		int a=130;
+//		byte b=(byte)a;
+//		System.out.println(a);
+//		System.out.println(b);
+		
+		// unary operator
+		
+//		int a=10;
+//		System.out.println(a);
+//		System.out.println(++a);
+//		System.out.println(++a);
+//	
+		//arithmetic operator
+		System.out.println(10+10/5*3-1*4/2);
+	}
+
+}
